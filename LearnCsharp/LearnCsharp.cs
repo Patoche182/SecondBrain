35c70ba9cb2a8c650094fd4aa1ab7651501e1ccf
PatriceHANNE <patrice.hanne@gmail.com>
2024-02-24 15:46:04 +0100

    Ajoutez des fichiers projet.

---
+++ LearnCsharp/LearnCsharp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnCsharp
+{
+    class Program
+    {
+        static void Main(string[] args) // Fonction Main point de départ du Program
+        {
+            Console.WriteLine("Hello Udemy");
+        }
+    }
+}

9222073f562b98e5678a1b64535396b87c7ce722
PatriceHANNE <patrice.hanne@gmail.com>
2024-02-25 16:49:37 +0100

    LEARN : Add Les Boucles

---
+++ Les Boucles/Boucle.cs
@@ -0,0 +1,12 @@
+﻿namespace Les_Boucles
+{
+    internal class Boucle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========-==========");
+            Console.WriteLine("Boucles!");
+            Console.WriteLine("==========-==========");
+        }
+    }
+}

00ab22645d5d6a633ec684d5fc6f553f010f6f41
PatriceHANNE <patrice.hanne@gmail.com>
2024-03-26 23:54:16 +0100

    Update LaPOO.cs

--- LaPOO/LaPOO.cs
+++ LaPOO/LaPOO.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Ceci est de la Programmation Orienté Objet");
-            Console.WriteLine("Ceci est de la Programmation Orienté Objet");
-            Console.WriteLine("Ceci est de la Programmation Orienté Objet");
+            Console.WriteLine("Ceci est de la Programmation Orienté Objet-2");
+            Console.WriteLine("Ceci est de la Programmation Orienté Objet-3");
         }
     }
 }

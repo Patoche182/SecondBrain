bc9a1c416467fb7bb0ef4d018557d3d0a4952e71
Patrice HANNE <patrice.hanne@gmail.com>
2024-04-01 23:30:58 +0200

    again

---
+++ LaPOO/POO_Externe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaPOO.LaPOO;
+
+
+class POO_Externe
+{
+    static void Main()
+    {
+
+        A_public a = new();
+
+        A_internal a_Internal = ne-w();
+    }
+}

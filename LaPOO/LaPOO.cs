4ac6bc755fbaa0c18cf89a1453aa8f2d0d995ad5
Patrice HANNE <patrice.hanne@gmail.com>
2024-04-01 23:18:57 +0200

    ADD : Stuff

--- LaPOO/LaPOO.cs
+++ LaPOO/LaPOO.cs
@@ -1,12 +1,47 @@
-﻿namespace LaPOO
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+class LaPOO
 {
-    internal class LaPOO
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine("Ceci est de la Programmation Orienté Objet");
+
+    //    A_internal objet_a = new A_internal();
+    //    A_internal objet_b = new A_internal();
+
+    //    objet_a.x = 1;
+
+    //    objet_a.Log();
+    //    objet_b.Log();
+    //    A_internal.y = 1;
+    //    objet_a.Log();
+    //    objet_b.Log();
+    //}
+
+    internal class A_internal
     {
-        static void Main(string[] args)
+        public int x = 0;
+        static public int y = 0;
+
+        public void Log()
         {
-            Console.WriteLine("Ceci est de la Programmation Orienté Objet");
-            Console.WriteLine("Ceci est de la Programmation Orienté Objet");
-            Console.WriteLine("Ceci est de la Programmation Orienté Objet");
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
         }
     }
+    public class A_public
+    {
+        public int x = 0;
+        static public int y = 0;
+
+        public void Log()
+        {
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
+        }
+    }
+
+
+
 }
